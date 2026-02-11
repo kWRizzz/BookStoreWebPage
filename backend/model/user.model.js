46d6410b481e6e86a6db074ade373ea4ac7ddff6
kWRizzz <krishnathey2j@gmail.com>
2026-02-11 23:28:06 +0530

    added login in backend and started frintend

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -16,12 +16,66 @@ const userSchema= mongoose.Schema(
             type:String,
             required:true
         },
-        userProfile:{
+        avatar:{
             type:String,
             default:""
-        }
+        },
+        mobile:{
+            type:Number
+        },
+        refreshToken:{
+            type:String,
+            default:""
+        },
+        verify_email:{
+            type:Boolean,
+            default:false
+        },
+        last_login:{
+            type:Date,
+            default:""
+        },
+        status:{
+            type:String,
+            enum:["Active","Inactive","Suspended"],
+            default:"Active"
+        },
+        address_details:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"adress"
+            }
+        ],
+        shoppingCart:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"cartProduct"
+            }
+        ],
+        orderHistory:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"order"
+            }
+        ],
+        forgotPassword:{
+            type:String,
+            default:null
+        },
+        forgotPasswordExpiry:{
+            type:Date,
+            default:""
+        },
+        role:{
+            type:String,
+            enum:["ADMIN","USER"],
+            default:"USER"
+        },
+       
+
     },
-    {
+    {   
+        
         timestamps:{
             type:Date,
             default:Date.now()
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cookieparser=require('cookie-parser')
 const dotenv=require('dotenv')
 const authRouter=require('./router/auth.router')
 const connectDB=require('./services/db')
+const cors= require('cors')
+const morgan=require('morgan')
+const helmet=require('helmet')
 
 dotenv.config()
 connectDB()
@@ -13,6 +16,14 @@ const PORT=process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({
     extended:true
+}))
+app.use(cors({
+    credentials:true,
+    // origin:process.env.FRONTEND_URL
+}))
+app.use(morgan())
+app.use(helmet({
+    crossOriginResourcePolicy:false
 }))
 app.use(cookieparser())
 app.use('/user/api',authRouter)
